013c9986fd3eb2135189b52c2eb9093dc4f9f17e
Piotr Chmielewski <pchmielewski6@gmail.com>
2019-06-15 11:37:39 +0200

    added e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('workspace-project HurtpolApp', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,24 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to portal-app!');
+    expect(page.getParagraphText('text-title')).toEqual('Hurtownia sprzętu komputerowego');
   });
+
+  it('should display warning message after unsuccessful login', () => {
+    page.navigateTo('login');
+    page.fillLoginForm('jan.kowalski@mail.com', 'PasswordXYZ')
+    expect(page.getTextByElementTag('mat-error')).toEqual('Email lub hasło jest niepoprawne. Spróbuj jeszcze raz')
+  })
+
+  it('should display redirect to home page after successful login', () => {
+    page.navigateTo('login');
+    page.fillLoginForm('jan.kowalski@mail.com', 'Password1!')
+    page.getCurrentUrl().then(url => {
+      const elements = url.split('/')
+      url = elements[elements.length-1]
+      expect(url).toEqual('home')
+    })
+  })
+
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,29 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, promise } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(url?: string) {
+    return browser.get( url ? `/${url}` : '/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText(className: string) {
+    return element(by.className(className)).getText();
+  }
+
+  getElementByCss(cssSelector: string) {
+    return element(by.css(cssSelector));
+  }
+
+  getTextByElementTag(tagName: string) {
+    return element(by.tagName(tagName)).getText();
+  }
+  
+  fillLoginForm(username: string, password: string) {
+    this.getElementByCss('[placeholder = "Hasło"]').sendKeys(password);
+    this.getElementByCss('[placeholder = "Login"]').sendKeys(username);
+    element(by.css('.button-primary')).click();
+  }
+
+  getCurrentUrl() : promise.Promise<string> {
+    return browser.getCurrentUrl();
   }
 }
